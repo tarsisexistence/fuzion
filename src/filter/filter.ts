a1a77766c31a8720ed29967eb119adc943dffa2a
tarsinzer <tarsis.maksym@gmail.com>
2022-08-28 17:33:42 +0100

    feat: typed filter operator

--- src/filter/filter.ts
+++ src/filter/filter.ts
@@ -3,12 +3,18 @@ import { Kind } from '../common';
 export type FilterFn<T> = (value: T, index: number) => boolean;
 export type Filter<T> = {
   kind: Kind.FILTER;
-  run: FilterFn<T>;
+  run: (value: T, index: number) => T;
 };
 
-export function filter<T>(predicate: FilterFn<T>): Filter<T> {
+// export function filter<T, S extends T>(
+//   predicate: (value: T, index: number) => value is S
+// ): S;
+export function filter<T>(
+  predicate: (value: T, index: number) => boolean
+): Filter<T> {
   return {
     kind: Kind.FILTER,
-    run: predicate
+    // TODO: refactor type coercion
+    run: predicate as (value: T, index: number) => T
   };
 }
